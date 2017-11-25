c1d19aa04b72c8e0013ba0a5a91575ae30b5597e
jrougemo <jacques.rougemont@epfl.ch>
2017-11-25 15:05:12 +0100

    my mistake...

---
+++ SimpleProject/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/rougemont/Documents/teaching/progCpp/2017/slides/code/ExampleProject/SimpleProject
+# Build directory: /home/rougemont/Documents/teaching/progCpp/2017/slides/code/ExampleProject/SimpleProject/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(project0 "project_test")
